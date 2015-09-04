f247b9ea7728733eb7a76a9ee2e797b6542a5f34
Guillaume Simon <ipernet@gmail.com>
2015-09-04 11:14:36 +0200

    Saving files..

--- app/index.js
+++ app/index.js
@@ -8,28 +8,17 @@ module.exports = yeoman.generators.Base.extend({
 
 		this.sourceRoot(path.join(__dirname, 'templates', 'dist'));
 
-		this.prompt([{
-			type: 'confirm',
-			name: 'docs',
-			message: 'Would you like docs included?',
-			default: false
-		}], function (props) {
-			if (props.docs) {
-				this.directory('doc');
-			}
+        this.directory('css');
+        this.directory('img');
+        this.directory('js');
 
-			this.directory('css');
-			this.directory('img');
-			this.directory('js');
+        this.expandFiles('*', {
+            cwd: this.sourceRoot(),
+            dot: true
+        }).forEach(function (el) {
+            this.copy(el);
+        }, this);
 
-			this.expandFiles('*', {
-				cwd: this.sourceRoot(),
-				dot: true
-			}).forEach(function (el) {
-				this.copy(el);
-			}, this);
-
-			cb();
-		}.bind(this));
+        cb();
 	}
 });
